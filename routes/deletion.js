bbdcfe33609aa82e2d6bfbdedc6a9e07b090b333
cseligevi <csenge200815@gmail.com>
2026-01-12 18:27:52 +0100

    Implement delete-all route for data removal
    Added a route to delete all entries and user data.

--- routes/deletion.js
+++ routes/deletion.js
@@ -7,3 +7,22 @@ router.post('/delete-all', (req, res) => {
   `);
   res.destroy();
 });
+const express = require('express');
+const db = require('../db');
+const router = express.Router();
+
+router.post('/delete-all', (req, res) => {
+  db.exec(`
+    DELETE FROM daily_entry;
+    DELETE FROM weekly_reflection;
+    DELETE FROM monthly_reflection;
+    DELETE FROM lifeos;
+    DELETE FROM user;
+  `);
+
+  req.session.destroy(() => {
+    res.redirect('/login');
+  });
+});
+
+module.exports = router;
