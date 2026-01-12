9f736ca41f9651fd5ba3c4e76a2e583151d021da
cseligevi <csenge200815@gmail.com>
2026-01-12 20:20:49 +0100

    Delete middleware.js

--- middleware.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (req, res, next) {
-  const openPaths = ['/login'];
-
-  if (!req.session.user && !openPaths.includes(req.path)) {
-    return res.redirect('/login');
-  }
-
-  next();
-};
